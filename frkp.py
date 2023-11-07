1047cb51882d5e9ffc3d7b619a9cb7f06461fc77
adityaoncloud <124081374+adityaoncloud@users.noreply.github.com>
2023-11-07 22:14:15 +0530

    Update frkp.py

--- frkp.py
+++ frkp.py
@@ -54,3 +54,58 @@ def fractional_knapsack():
 
 if __name__=="__main__":
     fractional_knapsack()
+
+
+
+import time
+start = time.time()
+
+def fractional_knapsack(items, capacity):
+    algo_start = time.time()
+    # Calculate the value-to-weight ratio for each item
+    item_value_ratio = [(item[1] / item[0], item) for item in items]
+    
+    # Sort items by value-to-weight ratio in descending order
+    item_value_ratio.sort(reverse=True)
+    
+    total_value = 0  # Total value of selected items
+    knapsack = []    # Items selected for the knapsack
+    
+    for value_per_weight, item in item_value_ratio:
+        if capacity >= item[0]:  # If the item can fit entirely
+            knapsack.append((item, 1))  # Add the whole item
+            total_value += item[1]
+            capacity -= item[0]
+        else:  # If the item can only fit partially
+            fraction = capacity / item[0]
+            knapsack.append((item, fraction))  # Add a fraction of the item
+            total_value += item[1] * fraction
+            break  # The knapsack is now full
+    algo_end = time.time()
+    algo_exec = (algo_end-algo_start)*10**3
+    
+    return total_value, knapsack, algo_exec
+
+# Example usage:
+# (X, Y): Where X = Item Weight, Y = Item Profit
+items = []
+
+n = int(input("How many items to add: "))
+for i in range(n):
+    print("\nEnter details for item-{}".format(i+1))
+    item_w = int(input("Enter item weight: "))
+    item_p = int(input("Enter item profit: "))
+    items.append((item_w, item_p))
+    
+print("\nItems are: {}".format(items))
+capacity = int(input("Enter capacity: "))
+max_value, selected_items, algo_execution_time = fractional_knapsack(items, capacity)
+
+print("\nMaximum value: {}".format(max_value))
+print("Selected items:")
+for item, fraction in selected_items:
+    print("  {}: Fraction = {}".format(item,round(fraction,2)))
+
+end = time.time()
+print("\nAlgorithm Execution time is: {}ms".format(algo_execution_time))
+print("Program Execution time is: {}ms".format((end-start)*10**3))
