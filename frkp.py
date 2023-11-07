5bc63cd0c7d03b7c9fdd60a976d09fe8c06df14a
adityaoncloud <124081374+adityaoncloud@users.noreply.github.com>
2023-11-07 13:09:04 +0530

    Update frkp.py

--- frkp.py
+++ frkp.py
@@ -17,3 +17,40 @@ def fractional_knapsack():
 
 if __name__=="__main__":
     fractional_knapsack()
+
+
+def fractional_knapsack():
+    weights = []
+    values = []
+# number of elements as input
+    n = int(input("Enter number of values : "))
+ 
+# iterating till the range
+    for i in range(0, n):
+        ele = int(input())
+        # adding the element
+        values.append(ele)  
+        
+    m = int(input("Enter number of weights : "))
+# iterating till the range
+    for j in range(0, m):
+        wei = int(input())
+        # adding the element
+        weights.append(wei)  
+    
+    capacity=50
+    res=0
+    # Pair : [Weight,value]
+    for pair in sorted(zip(weights,values), key= lambda x: x[1]/x[0], reverse=True):
+        if capacity<=0: # Capacity completed - Bag fully filled 
+            break 
+        if pair[0]>capacity: # Current's weight with highest value/weight ratio Available Capacity
+            res+=int(capacity * (pair[1]/pair[0]))  # Completely fill the bag
+            capacity=0
+        elif pair[0]<=capacity: # Take the whole object
+            res+=pair[1]
+            capacity-=pair[0]
+    print(res)        
+
+if __name__=="__main__":
+    fractional_knapsack()
